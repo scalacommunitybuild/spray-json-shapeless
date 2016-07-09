64ed122a0a862661075f90ae098d04c8e4f8cce6
Sam Halliday <sam.halliday@gmail.com>
2016-07-09 13:03:40 +0100

    bump version (new features)

--- project/SprayJsonShapelessBuild.scala
+++ project/SprayJsonShapelessBuild.scala
@@ -3,19 +3,20 @@
 import sbt._
 import Keys._
 import SonatypeSupport._
+import com.typesafe.tools.mima.plugin.MimaKeys._
 
 object SprayJsonShapelessBuild extends Build {
 
   lazy override val settings = super.settings ++ Seq(
     scalaVersion := "2.11.8",
     organization := "com.github.fommil",
-    version := "1.2.1-SNAPSHOT"
+    version := "1.3.0-SNAPSHOT"
   )
 
-  lazy val root = Project("root", file(".")).settings(
+  lazy val root = Project("spray-json-shapeless", file(".")).settings(
     Sensible.settings ++ sonatype("fommil", "spray-json-shapeless", Apache2)
   ).settings(
-    name := "spray-json-shapeless",
+    mimaPreviousArtifacts := Set(organization.value %% name.value % "1.2.0"),
     updateOptions := updateOptions.value.withCachedResolution(true),
     libraryDependencies ++= Seq(
       "io.spray" %% "spray-json" % "1.3.2",
